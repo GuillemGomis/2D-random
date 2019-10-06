81d1920f5668b5c22c3dabbc4e3e13217747dfa9
GuillemGomis <50328674+GuillemGomis@users.noreply.github.com>
2019-10-06 17:16:01 +0200

    Initial commit

---
+++ Assets/Menu.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+
+    public GameObject OptionsMenu;
+
+    public AudioMixer mixer;
+
+    Resolution[] resolutions;
+
+    public Dropdown resolutionDropdown;
+
+
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetVolume (float volume)
+    {
+        mixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void ChangeScene(int changeToScene)
+    {
+        SceneManager.LoadScene(changeToScene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce;
+    public bool IsGrounded = false;
+    private bool facingRight = true;
+    private float moveInput;
+    private Rigidbody2D rb;
+    private int extraJump;
+	
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    
+    void FixedUpdate()
+    {
+        
+        moveInput = Input.GetAxis("Horizontal");
+        
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+
+    void Update ()
+    {
+        if (IsGrounded == true)
+        {
+            extraJump = 1;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && extraJump > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJump--;
+        }
+      
+        
+
+
+    }
+
+   
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+       
+    }
+        
+
+}
